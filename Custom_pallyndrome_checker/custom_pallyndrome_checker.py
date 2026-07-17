7f9c0aca41ab55fba97a83ae6c1ced606d58b1f6
Samiddha Ganguly <rfordaredevils@gmail.com>
2026-07-17 23:10:39 +0530

    Update custom_pallyndrome_checker.py

--- Custom_pallyndrome_checker/custom_pallyndrome_checker.py
+++ Custom_pallyndrome_checker/custom_pallyndrome_checker.py
@@ -1,46 +1,45 @@
 # custom pallyndrome program for python program (string version)
 def string_pallyndrome_checker(string_custom):
     d = list(string_custom.lower())
-    e =list( reversed(d))
+    e = list(reversed(d))
     if d == e:
-        print("the string given by the user is a palyndrome")
+        return "the string given by the user is a palyndrome"
     else:
-        print("the string given by the user is not a pallyndrome")
+        return "the string given by the user is not a pallyndrome"
 
-# custom pallyndrome program for python program ( number version)
+# custom pallyndrome program for python program (number version)
 def number_pallyndrome_checker(custom_num):
     d = str(custom_num)
     e = list(d)
     f = list(reversed(e))
     if e == f:
-        print("the number given is a pallyndrome")
+        return "the number given is a pallyndrome"
     else:
-        print("the number given is not a pallyndrome")
+        return "the number given is not a pallyndrome"
 
-# main program body 
-def main(): 
- print("Choose from the following to run the string pallyndrome checker or the integer pallyndrome checker")
- print("for string pallyndrome checker type -------->     string_pal")
- print("for number pallyndrome checker type ------------->   num_pal")
- operator = input("enter the type you want to discover today!!")
- if operator.lower() == "string_pal":
-     string_custom = input("Enter your desired string over here ! ")
-     string_pallyndrome_checker(string_custom)
- elif operator.lower() == "num_pal":
-     custom_num = int(input("Enter your desired number over here"))
-     number_pallyndrome_checker(custom_num)
- else:
-     print("wrong user input")
+# main program body
+def main():
+    print("Choose from the following to run the string pallyndrome checker or the integer pallyndrome checker")
+    print("for string pallyndrome checker type -------->     string_pal")
+    print("for number pallyndrome checker type ------------->   num_pal")
+    operator = input("enter the type you want to discover today!!")
+    if operator.lower() == "string_pal":
+        string_custom = input("Enter your desired string over here ! ")
+        print(string_pallyndrome_checker(string_custom))
+    elif operator.lower() == "num_pal":
+        custom_num = int(input("Enter your desired number over here"))
+        print(number_pallyndrome_checker(custom_num))
+    else:
+        print("wrong user input")
 
 main()
 
-# main iterative body 
-
+# main iterative body
 for i in range(5):
-    choice = input("Wish to continue ? ----> yes/no")
+    choice = input("Wish to continue ? ----> yes/no").strip()
     if choice.lower() == "yes":
         main()
     elif choice.lower() == "no":
-        break 
+        break
     else:
         print("user choice not given")
